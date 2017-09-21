158de179e59ad2f7dd3258466e5faaa3c3f101c3
astronarh <astronarh@gmail.com>
2017-09-21 17:04:15 +0300

    Add file for upload data from SQL

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 
 public class Main {
     public static void main(String... args){
-        LoadFile.load("D:\\projects\\Bicycles&Wheels\\src\\main\\java\\files\\ToLoad.java", "D:\\projects\\Bicycles&Wheels\\src\\main\\java\\files\\comments.txt", "user");
+        //LoadFile.load("D:\\projects\\Bicycles&Wheels\\src\\main\\java\\files\\ToLoad.java", "D:\\projects\\Bicycles&Wheels\\src\\main\\java\\files\\comments.txt", "user");
+        ReadFile.read();
     }
 }
--- src/main/java/ReadFile.java
+++ src/main/java/ReadFile.java
@@ -1,4 +1,27 @@
+import java.io.*;
+import java.sql.*;
 
 public class ReadFile {
 
+    public static void read() {
+        try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/forjava?user=root&password=");) {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("select * from bicycles where id=7");
+            if (resultSet.next()) {
+                String name = resultSet.getString("file_name");
+                Blob file = resultSet.getBlob("file_contents");
+                InputStream x = file.getBinaryStream();
+                int size = x.available();
+                byte b[] = new byte[size];
+                x.read(b);
+                try (OutputStream targetFile = new FileOutputStream("C:\\Users\\Nurzhan\\Desktop\\Новая папка\\" + name)) {
+                    targetFile.write(b);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        } catch (SQLException | IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
